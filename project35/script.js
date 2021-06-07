1005c67b15b2929253649a32eda8e868a1c7a260
DeonJ76 <deon.jordaan76@gmail.com>
2021-06-07 20:11:43 +0200

    Project 35 complete

---
+++ project35/script.js
@@ -0,0 +1,42 @@
+const imageContainer = document.getElementById('images');
+const leftButton = document.getElementById('left');
+const rightButton = document.getElementById('right');
+
+const image = document.querySelectorAll('#images img');
+
+let index = 0;
+
+let interval = setInterval(run, 2000);
+
+function run() {
+	index++;
+
+	changeImage();
+}
+
+function changeImage() {
+	if (index > image.length - 1) {
+		index = 0;
+	} else if (index < 0) {
+		index = image.length - 1;
+	}
+
+	imageContainer.style.transform = `translateX(${-index * 500}px)`;
+}
+
+function resetInterval() {
+	clearInterval(interval);
+	interval = setInterval(run, 2000);
+}
+
+rightButton.addEventListener('click', () => {
+	index++;
+	changeImage();
+	resetInterval();
+});
+
+leftButton.addEventListener('click', () => {
+	index--;
+	changeImage();
+	resetInterval();
+});
