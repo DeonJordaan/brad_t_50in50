9837500eab9b40c2465167c38961c8ef726819af
DeonJ76 <deon.jordaan76@gmail.com>
2021-06-17 22:08:16 +0200

    Project 12 complete

---
+++ project12/script.js
@@ -0,0 +1,7 @@
+const toggleButtons = document.querySelectorAll('.faq-toggle');
+
+toggleButtons.forEach((btn) =>
+	btn.addEventListener('click', () => {
+		btn.parentNode.classList.toggle('active');
+	})
+);
